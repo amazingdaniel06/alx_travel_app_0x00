3faf248a80d1d9311eea90cadac38ef0d77b9d9c
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-20 23:07:22 +0000

    Update serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,23 @@
+from rest_framework import serializers
+from .models import Listing, Booking, Review
 
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    bookings = BookingSerializer(many=True, read_only=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = ['id', 'title', 'description', 'location', 'price_per_night', 'created_at', 'bookings', 'reviews']
